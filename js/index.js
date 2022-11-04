4ea3fe5256a58f9e98a7ac76a72058de81a97268
Marufjon777 <Marufturdaliyev777@gmail.com>
2022-11-04 12:12:48 +0000

    playlist qoshildi

--- js/index.js
+++ js/index.js
@@ -1,18 +1,45 @@
-let list =document.querySelector("#list")
-let button =document.querySelector("#btn")
-let img= document.querySelector(".corusel_item");
+let list = document.querySelector("#list");
+let button = document.querySelector("#btn");
+let img = document.querySelector(".corusel_item");
 let left_btn = document.querySelector(".left_btn");
 let right_btn = document.querySelector(".right_btn");
-let corusel_item = document.querySelector(".corusel_item");
-let images= ["https://i.ytimg.com/vi/FAg8JZwFM2U/maxresdefault.jpg", 
-"https://peoplenews.uz/wp-content/uploads/2021/07/image-72.jpeg",
-"https://uzreport.news/fotobank/image/aa6ce48e83ad2885b47825b9942048ab.jpeg",
-"https://i.pinimg.com/736x/18/bd/41/18bd4134927a899db79cb9ea64643838.jpg"]
+let corusel_item = document.querySelectorAll(".corusel_item");
+
 
 button.addEventListener("click" , ()=>{
-   if(list.classList[1]==="toggle-list"){
-    list.classList.remove("toggle-list")
-   }else{
-    list.classList.add("toggle-list")
-   }
+    if(list.classList[1]==="toggle_list"){
+        list.classList.remove("toggle_list")
+    }else{
+        list.classList.add("toggle_list")
+    }
 })
+
+
+let slider = document.querySelector('.slider');
+let ul = slider.querySelector('#ul');
+let li =document.querySelectorAll("#ul li");
+let leftBtn = document.querySelector('#left');
+let rightBtn = document.querySelector('#right');
+let score = 0;
+let width = li[0].clientWidth
+
+
+
+leftBtn.addEventListener('click', ()=>{
+    if((li.length-2)*width+score<0){
+        score=0
+        ul.style.marginLeft=`${score}px`;
+    }else{
+        ul.style.marginLeft=`${score-=width}px`;
+    }
+    
+});
+rightBtn.addEventListener('click', ()=>{
+    if((li.length-2)*width+score>0){
+        score=0
+        ul.style.marginLeft=`${score}px`;
+    }else{
+        ul.style.marginLeft=`${score+=width}px`;
+    }
+    
+});
